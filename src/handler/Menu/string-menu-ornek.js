492b7d9a8a0f8d869b1b1fe9cca9e05f7732e897
Cihqt <yestefir@gmail.com>
2025-04-06 04:57:09 +0300

    ✨

--- src/handler/Menu/string-menu-ornek.js
+++ src/handler/Menu/string-menu-ornek.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     value: 'add_user_to_ticket', // Örnek Value
-    type: 'USER_SELECT_MENU', // Bu önemli!
+    type: 'USER_SELECT_MENU', // Bu önemli! (USER_SELECT_MENU adı üstünde rol üye seçimli özel menülerde bunu koyuyoruz bunada özel bir handler gelebilir.) 
     
     async execute(interaction) {
         if (!interaction.isUserSelectMenu()) return;
